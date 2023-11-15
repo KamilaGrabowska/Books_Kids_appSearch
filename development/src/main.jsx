71c85500694fcc1dff5727000b1b775b446c77a9
KAMILA <dudekkamila@icloud.com>
2023-11-15 23:37:11 +0100

    added main funcionality to app - search books

--- development/src/main.jsx
+++ development/src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BooksSearch from "./assets/BooksSearch.jsx";
+import "./assets/BSearch.scss"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        {<BooksSearch />}
+    </React.StrictMode>
+);
