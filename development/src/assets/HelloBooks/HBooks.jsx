809555619698ea109289c882a3cc5d27a1a03357
KAMILA <dudekkamila@icloud.com>
2023-11-16 23:11:34 +0100

    added HelloBooks compomenets

---
+++ development/src/assets/HelloBooks/HBooks.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const HBooks = () => {
+    const [text, setText] = useState("Witaj MAŁY czytelniku");
+    const timerRef = useRef();
+
+    useEffect(() => {
+        timerRef.current = setTimeout(() => {
+            setText("Zapraszam do lektury");
+        }, 10000);
+
+        return () => {
+            console.log('DIE');
+            clearTimeout(timerRef.current);
+        };
+    }, []);
+    return <h2>{text}</h2>;
+};
+
+export default HBooks;
+
+
+
+
+
--- development/src/main.jsx
+++ development/src/main.jsx
@@ -2,10 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import BooksSearch from "./assets/BooksSearch.jsx";
 import "./assets/BSearch.scss"
+import HBooks from "./assets/HelloBooks/HBooks.jsx"
+import "./assets/HelloBooks/HBooks.scss"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
+        {<HBooks />}
         {<BooksSearch />}
+
     </React.StrictMode>
 );
+
+
+
+
+// import React from "react";
+// import {createRoot} from "react-dom/client";
+// import BooksSearch from "./assets/BooksSearch.jsx";
+// import "./assets/BSearch.scss"
+//
+// const container = document.getElementById("app");
+// const root = createRoot(container);
+//
+//  const App = ()=>{
+//      return (
+//          <>
+//              <BooksSearch />
+//          </>
+//      )
+//  }
+//
+//  App();
